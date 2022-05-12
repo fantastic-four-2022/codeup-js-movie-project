c79af64bb869880ea7302557a2b99a82d054f2c3
David Prieto <david.s.prieto1@gmail.com>
2022-05-12 10:43:41 -0500

    Added JS to allow user to add a new movie. Working on Allowing user to delete movies.

--- movie-app.js
+++ movie-app.js
@@ -7,7 +7,7 @@
 //     },
 //     body: JSON.stringify(blogPost),
 // };
-// var url = 'https://codeup-json-server.glitch.me/movies'
+const url = 'https://codeup-json-server.glitch.me/movies'
 fetch('https://codeup-json-server.glitch.me/movies')
     .then(res => res.json())
     .then(data => {
@@ -18,10 +18,11 @@ fetch('https://codeup-json-server.glitch.me/movies')
 
             // function hasTitle(){
             //     if(post.title !== 'undefined'){
-                    $('#titles').append(
-                        `<div>${post.title}</div>`
-                        // +
-                        // `<button>delete me</button>`
+                    $('#movies').append(
+                        `<h1>${post.title}</h1>` +
+                        `<h4>Rating:  ${post.rating}</h4>` +
+                        `<img src="${post.poster}" style="height:300px; width:200px;">` +
+                        `<button id="delete">delete movie</button>`
                     )
             }
             //     }
@@ -29,23 +30,82 @@ fetch('https://codeup-json-server.glitch.me/movies')
             // return hasTitle()
         })
     })
-const reviewObj = {
-    title: 1,
-    director: 'Codey',
-    rating: 5,
-    genre: "This is a really good place for coding and eating"
-};
-const url = 'https://codeup-json-server.glitch.me/movies';
-const options = {
-    method: 'POST',
+// function addMovie() {
+//     const reviewObj = {
+//         id: movies.length + 1,
+//         title: document.getElementById(),
+//         director: 'Codey',
+//         rating: 5,
+//         genre: "This is a really good place for coding and eating"
+//     };
+//     const url = 'https://codeup-json-server.glitch.me/movies';
+//     const options = {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(reviewObj),
+//     };
+//     fetch(url, options)
+//         .then(response => console.log(response)) /* review was created successfully */
+//         .catch(error => console.error(error)); /* handle errors */
+// }
+
+
+$('#add').click((e) => {
+    e.preventDefault();
+
+    var addMovie = {
+        title: $("#title").val(),
+        genre: $("#genre").val(),
+        rating: $("#rating").val(),
+        director: $("#director").val(),
+        plot: $("#plot").val()
+    }
+    // const url = 'https://codeup-json-server.glitch.me/movies';
+    let options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(addMovie),
+    };
+    fetch(url, options)
+        .then(response => console.log(response)) /* review was created successfully */
+        .catch(error => console.error(error)); /* handle errors */
+})
+
+
+let deleteOptions = {
+    method: 'DELETE',
     headers: {
         'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(reviewObj),
+    }
 };
-fetch(url, options)
-    .then( response => console.log(response) ) /* review was created successfully */
-    .catch( error => console.error(error) ); /* handle errors */
+
+$("#delete").click(function() {
+//DELETE request
+    fetch(`${url}/${inputVal}`, deleteOptions)
+        .then();
+});
+
+// $('#delete').click((e) => {
+//     e.preventDefault();
+
+// let options = {
+//     method: 'DELETE',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(),
+// };
+// fetch(url, options)
+//     .then(response => console.log(response)) /* review was created successfully */
+//     .catch(error => console.error(error)); /* handle errors */
+// })
+
+
+
 
 // $('button').click($(this) => console.log($(this).siblings))
 
